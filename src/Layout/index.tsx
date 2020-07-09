758927b940fc93b43d3fb04a4c679cf2031b63e5
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-07-09 17:45:30 -0300

    add hover effect to titles

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -4,16 +4,13 @@ import styled from "styled-components";
 
 const StyledTitle = styled.h3`
   border-bottom: 3px solid transparent;
-  transition: 0.1s;
+  transition: 0.2s;
   margin-bottom: 10px;
-  &:hover {
-    border-bottom: 3px solid white;
-  }
 `;
 
 const Layout = () => (
   <div className={classes.Grid}>
-    <div className={classes.Books}>
+    <div className={[classes.Books, classes.Item].join(" ")}>
       <StyledTitle>Books</StyledTitle>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quia
@@ -22,11 +19,23 @@ const Layout = () => (
         tempore. Saepe, nulla!
       </p>
     </div>
-    <div className={classes.Notes}>
+    <div className={[classes.Notes, classes.Item].join(" ")}>
       <StyledTitle>Notes</StyledTitle>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quia
+        placeat sit. Similique voluptatibus assumenda harum praesentium maxime
+        blanditiis error asperiores eum debitis! Earum culpa placeat voluptas
+        tempore. Saepe, nulla!
+      </p>
     </div>
-    <div className={classes.Tags}>
+    <div className={[classes.Tags, classes.Item].join(" ")}>
       <StyledTitle>Tags</StyledTitle>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quia
+        placeat sit. Similique voluptatibus assumenda harum praesentium maxime
+        blanditiis error asperiores eum debitis! Earum culpa placeat voluptas
+        tempore. Saepe, nulla!
+      </p>
     </div>
   </div>
 );
