10b98e95de914c53fc342c4e7e079e9cb588d884
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-07-09 14:52:45 -0300

    init

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    color: var(--white);
+  }
+
+  html, border-style, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  html {
+    background: var(--primary);
+  }
+
+  :root {
+    --primary: #04061c;
+    --white: #D9D9D9;
+  }
+`
